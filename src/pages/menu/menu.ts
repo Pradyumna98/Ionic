4175335a4a26a6dc1370a715a41fedc3a58960fd
Pradyumna98 <vknarayana98@gmail.com>
2020-08-20 18:44:23 +0530

    Advanced Lists

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -3,6 +3,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Dish } from '../../shared/dish';
 import { DishProvider } from '../../providers/dish/dish';
 import { DishdetailPage } from '../dishdetail/dishdetail';
+import { FavoriteProvider } from '../../providers/favorite/favorite';
+
 
 /**
  * Generated class for the MenuPage page.
@@ -22,8 +24,12 @@ export class MenuPage implements OnInit {
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     private dishservice: DishProvider,
+    private favoriteservice: FavoriteProvider,
     @Inject('BaseURL') private BaseURL) { }
-  
+
+
+
+    
   ngOnInit() {
     this.dishservice.getDishes()
       .subscribe(dishes => this.dishes = dishes,
@@ -40,4 +46,11 @@ export class MenuPage implements OnInit {
       dish: dish
     });
   }
+
+  addToFavorites(dish: Dish) {
+    console.log('Adding to Favorites', dish.id);
+    this.favoriteservice.addFavorite(dish.id);
+  }
+
+  
 }
