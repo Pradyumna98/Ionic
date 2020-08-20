ef9e5c0d1d303725cfa55e20d372be9328242b54
Pradyumna98 <vknarayana98@gmail.com>
2020-08-20 20:55:21 +0530

    Alerts, Toasts and Loading

--- src/pages/dishdetail/dishdetail.ts
+++ src/pages/dishdetail/dishdetail.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { Dish } from '../../shared/dish';
 import { Comment } from '../../shared/comment';
 import { FavoriteProvider } from '../../providers/favorite/favorite';
@@ -27,8 +27,9 @@ export class DishdetailPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     @Inject('BaseURL') private BaseURL,
-
-    private favoriteservice: FavoriteProvider) {
+    private favoriteservice: FavoriteProvider,
+    private toastCtrl: ToastController) {
+      
     this.dish = navParams.get('dish');
     this.favorite = favoriteservice.isFavorite(this.dish.id);
     this.numcomments = this.dish.comments.length; 
@@ -41,10 +42,15 @@ export class DishdetailPage {
     console.log('ionViewDidLoad DishdetailPage');
   }
 
-
+  
   addToFavorites() {
     console.log('Adding to Favorites', this.dish.id);
     this.favorite = this.favoriteservice.addFavorite(this.dish.id);
+     this.toastCtrl.create({
+    message: 'Dish ' + this.dish.id + ' added as favorite successfully',
+    position: 'middle',
+    duration: 3000}).present();
   }
 
+  
 }
